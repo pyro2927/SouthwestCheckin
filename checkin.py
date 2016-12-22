71e63f9bf813f61bf7d3c903c497543c87c71975
pyro2927 <joseph@pintozzi.com>
2016-12-22 14:34:55 -0700

    Making checkin script time-aware

--- checkin.py
+++ checkin.py
@@ -1,5 +1,11 @@
 import requests
 import sys
+from dateutil.parser import parse
+from datetime import datetime
+from datetime import timedelta
+import pytz
+from tzlocal import get_localzone
+import time
 
 # Pulled from proxying the Southwest iOS App
 headers = {'Host': 'mobile.southwest.com', 'Content-Type': 'application/vnd.swacorp.com.mobile.reservations-v1.0+json', 'X-API-Key': 'l7xxab4b3533b8d54bad9df230deb96a6f90', 'Accept': '*/*'}
@@ -14,6 +20,17 @@ url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record
 r = requests.get(url, headers=headers)
 body = r.json()
 
+departure_time = body['itinerary']['originationDestinations'][0]['segments'][0]['departureDateTime']
+
+date = parse(departure_time)
+tomorrow = datetime.now(pytz.utc).astimezone(get_localzone()) + timedelta(days=1)
+
+# Wait until checkin time
+if date > tomorrow:
+    delta = (date-tomorrow).total_seconds()
+    print "Too early to check in, waiting {} seconds".format(delta)
+    time.sleep(delta)
+
 # Get our passengers to get boarding passes for
 passengers = []
 for passenger in body['passengers']:
@@ -24,7 +41,12 @@ headers['Content-Type'] = 'application/vnd.swacorp.com.mobile.boarding-passes-v1
 url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record-locator/{}/boarding-passes".format(reservation_number)
 r = requests.post(url, headers=headers, json={'names': passengers})
 
-# Spit out info about boarding number
-for checkinDocument in r.json()['passengerCheckInDocuments']:
-    for doc in checkinDocument['checkinDocuments']:
-        print "You got {}{}!".format(doc['boardingGroup'], doc['boardingGroupNumber'])
+body = r.json()
+
+if body['httpStatusCode'] == 'FORBIDDEN':
+    print body['message']
+else:
+    # Spit out info about boarding number
+    for checkinDocument in body['passengerCheckInDocuments']:
+        for doc in checkinDocument['checkinDocuments']:
+            print "You got {}{}!".format(doc['boardingGroup'], doc['boardingGroupNumber'])
