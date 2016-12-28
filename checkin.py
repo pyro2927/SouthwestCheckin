da771b971783a3a81abe7c7753870447084d5f08
pyro2927 <joseph@pintozzi.com>
2016-12-28 16:15:19 -0600

    Adding check for unfound reservations

--- checkin.py
+++ checkin.py
@@ -21,35 +21,39 @@ url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record
 r = requests.get(url, headers=headers)
 body = r.json()
 
-departure_time = body['itinerary']['originationDestinations'][0]['segments'][0]['departureDateTime']
-
-date = parse(departure_time)
-tomorrow = datetime.now(pytz.utc).astimezone(get_localzone()) + timedelta(days=1)
-
-# Wait until checkin time
-if date > tomorrow:
-    delta = (date-tomorrow).total_seconds()
-    print "Too early to check in, waiting {} seconds".format(delta)
-    time.sleep(delta)
-
-print "Attempting check-in..."
-
-# Get our passengers to get boarding passes for
-passengers = []
-for passenger in body['passengers']:
-    passengers.append({'firstName': passenger['secureFlightName']['firstName'], 'lastName': passenger['secureFlightName']['lastName']})
-
-# Check in
-headers['Content-Type'] = 'application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json'
-url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record-locator/{}/boarding-passes".format(reservation_number)
-r = requests.post(url, headers=headers, json={'names': passengers})
-
-body = r.json()
-
-if 'httpStatusCode' in body and body['httpStatusCode'] == 'FORBIDDEN':
+# Confirm this reservation is found
+if 'httpStatusCode' in body and body['httpStatusCode'] == 'NOT_FOUND':
     print body['message']
 else:
-    # Spit out info about boarding number
-    for checkinDocument in body['passengerCheckInDocuments']:
-        for doc in checkinDocument['checkinDocuments']:
-            print "You got {}{}!".format(doc['boardingGroup'], doc['boardingGroupNumber'])
+    departure_time = body['itinerary']['originationDestinations'][0]['segments'][0]['departureDateTime']
+
+    date = parse(departure_time)
+    tomorrow = datetime.now(pytz.utc).astimezone(get_localzone()) + timedelta(days=1)
+
+    # Wait until checkin time
+    if date > tomorrow:
+        delta = (date-tomorrow).total_seconds()
+        print "Too early to check in, waiting {} seconds".format(delta)
+        time.sleep(delta)
+
+    print "Attempting check-in..."
+
+    # Get our passengers to get boarding passes for
+    passengers = []
+    for passenger in body['passengers']:
+        passengers.append({'firstName': passenger['secureFlightName']['firstName'], 'lastName': passenger['secureFlightName']['lastName']})
+
+    # Check in
+    headers['Content-Type'] = 'application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json'
+    url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record-locator/{}/boarding-passes".format(reservation_number)
+    r = requests.post(url, headers=headers, json={'names': passengers})
+
+    body = r.json()
+
+    if 'httpStatusCode' in body and body['httpStatusCode'] == 'FORBIDDEN':
+        print body['message']
+    else:
+        # Spit out info about boarding number
+        for checkinDocument in body['passengerCheckInDocuments']:
+            for doc in checkinDocument['checkinDocuments']:
+                print "You got {}{}!".format(doc['boardingGroup'], doc['boardingGroupNumber'])
