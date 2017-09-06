5732948e1990a72a7c764cca99444a7568b08c10
pyro2927 <joseph@pintozzi.com>
2017-09-05 23:03:22 -0700

    Adding pep8, formatting some long lines

--- checkin.py
+++ checkin.py
@@ -10,14 +10,19 @@ import time
 from math import trunc
 
 # Pulled from proxying the Southwest iOS App
-headers = {'Host': 'mobile.southwest.com', 'Content-Type': 'application/vnd.swacorp.com.mobile.reservations-v1.0+json', 'X-API-Key': 'l7xxab4b3533b8d54bad9df230deb96a6f90', 'Accept': '*/*'}
+headers = {'Host': 'mobile.southwest.com',
+           'Content-Type': 'application/vnd.swacorp.com.mobile.reservations-v1.0+json',
+           'X-API-Key': 'l7xxab4b3533b8d54bad9df230deb96a6f90',
+           'Accept': '*/*'}
 
 reservation_number = sys.argv[1]
 first_name = sys.argv[2]
 last_name = sys.argv[3]
 
+URL_BASE = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record-locator/"
+
 # Find our existing record
-url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record-locator/{}?first-name={}&last-name={}".format(reservation_number, first_name, last_name)
+url = "{}{}?first-name={}&last-name={}".format(URL_BASE, reservation_number, first_name, last_name)
 
 r = requests.get(url, headers=headers)
 body = r.json()
@@ -56,11 +61,12 @@ else:
     # Get our passengers to get boarding passes for
     passengers = []
     for passenger in body['passengers']:
-        passengers.append({'firstName': passenger['secureFlightName']['firstName'], 'lastName': passenger['secureFlightName']['lastName']})
+        passengers.append({'firstName': passenger['secureFlightName']['firstName'],
+                           'lastName': passenger['secureFlightName']['lastName']})
 
     # Check in
     headers['Content-Type'] = 'application/vnd.swacorp.com.mobile.boarding-passes-v1.0+json'
-    url = "https://mobile.southwest.com/api/extensions/v1/mobile/reservations/record-locator/{}/boarding-passes".format(reservation_number)
+    url = "{}{}/boarding-passes".format(URL_BASE, reservation_number)
     r = requests.post(url, headers=headers, json={'names': passengers})
 
     body = r.json()
