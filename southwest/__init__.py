b0056e75e26a2b50319e46c05acc4052c5aca677
Lockefox <locke.renard@gmail.com>
2021-08-18 13:43:51 -0700

    adds setup.py packaging for native python installing

--- southwest/__init__.py
+++ southwest/__init__.py
@@ -1,2 +1,7 @@
+import pkgutil
+
 from .southwest import Reservation
 from .openflights import timezone_for_airport
+
+__library_name__ = "southwest"
+__version__ = pkgutil.get_data(__library_name__, "VERSION").decode("utf-8")
